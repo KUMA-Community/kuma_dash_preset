619686defdefbf0fdf20b2bcf2a7a94750b5dffe
Boris <39199196+borross@users.noreply.github.com>
2024-12-06 15:56:02 +0300

    Update kuma_dash_preset.sh

--- kuma_dash_preset.sh
+++ kuma_dash_preset.sh
@@ -6,8 +6,10 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m'
 
+USER_LOGIN="admin"
 KUMA_VER=$(/opt/kaspersky/kuma/kuma version | cut -d "." -f1-2)
 ACTUAL_TENANT_ID=$(/opt/kaspersky/kuma/mongodb/bin/mongo localhost/kuma --quiet --eval 'db.tenants.find({"main":true},{"_id": 1}).map(function(doc){return doc._id;});' | sed "s/'/\"/g" | cut -d '"' -f2)
+ACTUAL_ADMIN_ID=$(/opt/kaspersky/kuma/mongodb/bin/mongo localhost/kuma --quiet --eval 'db.standalone_users.find({"login":"'$USER_LOGIN'"},{"_id": 1}).map(function(doc){return doc._id;});' | sed "s/'/\"/g" | cut -d '"' -f2)
 
 if [[ $(bc <<< "$KUMA_VER >= 3.2") -eq 1 ]]; then 
 	ACTUAL_CLUSTER_ID=$(/opt/kaspersky/kuma/mongodb/bin/mongo localhost/kuma --quiet --eval 'db.services.find({"kind": "storage", "status": "green", "tenantID": "'$ACTUAL_TENANT_ID'"})[0].resourceID')
@@ -17,15 +19,17 @@ else
 	mainID=$(/opt/kaspersky/kuma/mongodb/bin/mongo localhost/kuma --quiet --eval 'db.tenants.findOne({"main":true})._id');/opt/kaspersky/kuma/mongodb/bin/mongo localhost/kuma --quiet --eval 'db.dashboards.updateMany({"name": {$in:["[OOTB] KWTS","[OOTB] KSMG","[OOTB] KSC","[OOTB] KATA & EDR","Network Overview"]}}, {$set: {"widgets.$[x].search.clusterID": "'"$ACTUAL_CLUSTER_ID"'"}},{arrayFilters: [{"x.search.clusterID": {$in:["phantomID","","6c964dab-e303-4a2c-bb31-d84866a20599"]}, "x.specialKind": ""}]});'
 fi
 
-usage="\n$(basename "$0") [-h] [-exportDash] [-importDash] [-deleteDash] [-exportPreset] [-importPreset] <ARGUMENTS> -- program for executing script\n
+usage="\n$(basename "$0") [-h] [-exportDash] [-importDash] [-deleteDash] [-exportPreset] [-importPreset] [-exportSearch] [-importSearch] <ARGUMENTS> -- program for executing script\n
 \n
 where:\n
     ${YELLOW}-h${NC} -- show this help text\n
-    ${YELLOW}-exportDash \"<Dashboard Name>\" </path/File Export Name.json>${NC} -- export dashboard to file in JSON (dashboard name must be UNIQUE! and use \" if spaces are in the name)\n
+    ${YELLOW}-exportDash \"<Dashboard Name>\" </path/File Export Name.json>${NC} -- export dashboard to file in JSON (name must be UNIQUE! and use \" if spaces are in the name)\n
     ${YELLOW}-importDash <File Export Name.json>${NC} -- import dashboard to KUMA\n
     ${YELLOW}-deleteDash \"<Dashboard Name>\"${NC} -- delete dashboard from KUMA (dashboard name must be UNIQUE! and use \" if spaces are in the name)\n
-	${YELLOW}-exportPreset \"<Preset Name>\" </path/File Export Name.json>${NC} -- export Preset to file in JSON (dashboard name must be UNIQUE! and use \" if spaces are in the name)\n
+	${YELLOW}-exportPreset \"<Preset Name>\" </path/File Export Name.json>${NC} -- export Preset to file in JSON (name must be UNIQUE! and use \" if spaces are in the name)\n
     ${YELLOW}-importPreset <File Export Name.json>${NC} -- import Preset to KUMA\n
+	${YELLOW}-exportSearch \"<Search Name>\" </path/File Export Name.json>${NC} -- export Search to file in JSON (name must be UNIQUE! and use \" if spaces are in the name)\n
+    ${YELLOW}-importSearch <File Export Name.json>${NC} -- import Search to KUMA\n
 \n
 "
 
@@ -51,7 +55,7 @@ case $1 in
 	if [[ ! $# -eq 3 ]] || [[ $2 == ""  ]] || [[ $3 == ""  ]] || [[ $IS_EXPORT == 0 ]]; then
 		echo -e "${YELLOW}Please enter valid arguments!\nExample: -export \"My Dashboad\" MyDashboard.json\n OR check is jq installed?${NC}"
 	else
-		/opt/kaspersky/kuma/mongodb/bin/mongo localhost/kuma --quiet --eval 'db.dashboards.find({"name": "'$2'"}).forEach(function(doc){print(JSON.stringify(doc));});' | jq -c '._id="UUID" | .widgets[].tenantIDs=["T_ID"] | .tenantIDs=["T_ID"] | .widgets[].search.clusterID="C_ID" | del(.results)'> $3
+		/opt/kaspersky/kuma/mongodb/bin/mongo localhost/kuma --quiet --eval 'db.dashboards.find({"name": "'$2'"}).forEach(function(doc){print(JSON.stringify(doc));});' | jq -c '._id="UUID" | .widgets[].tenantIDs=["T_ID"] | .tenantIDs=["T_ID"] | .widgets[].search.clusterID="C_ID" | .widgets[].search.eventSpaceConfig.spacesByClusterID={"C_ID":{"spaceids": [""],"allspaces":false}} | .widgets[].search.clusterIDs[]=["C_ID"] | .userID="U_ID" | del(.results)'> $3
 		
 		if [[ $? -eq 0 ]]; then
 			echo -e "${GREEN}[OK] Dashboard ${2} exported to file $3!${NC}"
@@ -63,13 +67,20 @@ case $1 in
     ;;
 
     "-importDash")
-	if [[ ! $# -eq 2 ]] || [[ $2 == ""  ]] || [[ ! -f $2 ]]; then
+	if [[ ! $# -eq 2 ]] || [[ $2 == ""  ]] || [[ ! -f $2 ]] || [[ $(grep -c '"widgets"' "$2") -eq 0 ]]; then
 		echo -e "${YELLOW}Please enter valid arguments!\nExample: -import /path/MyDashboard.json${NC}"
         echo -e "${YELLOW}OR Please check file $2, is it exist?${NC}"
+		echo -e "$[FAILED] It is not dashboard file!${2}!${NC}"
 	else
         sed -i "s/T_ID/$ACTUAL_TENANT_ID/g" $2
         sed -i "s/C_ID/$ACTUAL_CLUSTER_ID/g" $2
+		sed -i "s/U_ID/$$ACTUAL_ADMIN_ID/g" $2
         sed -i "s/UUID/$(cat /proc/sys/kernel/random/uuid)/g" $2
+
+		#if [[ cat $2 | grep -c '"widgets"' -eq 0 ]]; then
+		#	echo -e "$[FAILED] It is not dashboard file!${2}!${NC}"
+		#	exit 1
+		#fi
 		
 		/opt/kaspersky/kuma/mongodb/bin/mongo localhost/kuma --quiet --eval 'db.dashboards.insertOne('"$2"');'
 		
@@ -99,7 +110,7 @@ case $1 in
 	if [[ ! $# -eq 3 ]] || [[ $2 == ""  ]] || [[ $3 == ""  ]] || [[ $IS_EXPORT == 0 ]]; then
 		echo -e "${YELLOW}Please enter valid arguments!\nExample: -exportPreset \"My Preset\" MyPreset.json\n OR check is jq installed?${NC}"
 	else
-		/opt/kaspersky/kuma/mongodb/bin/mongo localhost/kuma --quiet --eval 'db.resources.find({"kind":"eventPreset","name": "'$2'"}).forEach(function(doc){print(JSON.stringify(doc));});' | jq -c '._id="UUID" | .payload.tenantID="T_ID" | .tenantID="T_ID"'> $3
+		/opt/kaspersky/kuma/mongodb/bin/mongo localhost/kuma --quiet --eval 'db.resources.find({"kind":"eventPreset","name": "'$2'"}).forEach(function(doc){print(JSON.stringify(doc));});' | jq -c '._id="UUID" | .payload.tenantID="T_ID" | .tenantID="T_ID" | .payload.userID="U_ID" | .userID="U_ID" | .payload.id="UUID"'> $3
 		
 		if [[ $? -eq 0 ]]; then
 			echo -e "${GREEN}[OK] Preset ${2} exported to file $3!${NC}"
@@ -110,24 +121,65 @@ case $1 in
     ;;
 
 	"-importPreset")
-	if [[ ! $# -eq 2 ]] || [[ $2 == ""  ]] || [[ ! -f $2 ]]; then
+	if [[ ! $# -eq 2 ]] || [[ $2 == ""  ]] || [[ ! -f $2 ]] || [[ $(grep -c '"kind":"eventPreset"' "$2") -eq 0 ]]; then
 		echo -e "${YELLOW}Please enter valid arguments!\nExample: -importPreset \"My Preset\" MyPreset.json${NC}"
 		echo -e "${YELLOW}OR Please check file $2, is it exist?${NC}"
+		echo -e "$[FAILED] It is not preset file!${2}!${NC}"
 	else
 		sed -i "s/T_ID/$$ACTUAL_TENANT_ID/g" $2
+		sed -i "s/U_ID/$$ACTUAL_ADMIN_ID/g" $2
 		sed -i "s/UUID/$(cat /proc/sys/kernel/random/uuid)/g" $2
 
-		if [[ cat $2 | grep -c '"kind":"eventPreset"' -eq 0 ]]; then
-			echo -e "$[FAILED] It is not preset file!${2}!${NC}"
-			exit 1
-		fi
+		#if [[ cat $2 | grep -c '"kind":"eventPreset"' -eq 0 ]]; then
+		#	echo -e "$[FAILED] It is not preset file!${2}!${NC}"
+		#	exit 1
+		#fi
 		
 		/opt/kaspersky/kuma/mongodb/bin/mongo localhost/kuma --quiet --eval 'db.resources.insertOne('"$2"');'
 		
 		if [[ $? -eq 0 ]]; then
-			echo -e "${GREEN}[OK] from file ${2} imported!${NC}"
+			echo -e "${GREEN}[OK] Preset from file ${2} imported!${NC}"
 		else
-			echo -e "${RED}[FAILED] to import dashboard ${2}!${NC}"
+			echo -e "${RED}[FAILED] to import Preset ${2}!${NC}"
+		fi
+	fi
+    ;;
+
+	"-exportSearch")
+	if [[ ! $# -eq 3 ]] || [[ $2 == ""  ]] || [[ $3 == ""  ]] || [[ $IS_EXPORT == 0 ]]; then
+		echo -e "${YELLOW}Please enter valid arguments!\nExample: -exportSearch \"My Search\" MySearch.json\n OR check is jq installed?${NC}"
+	else
+		/opt/kaspersky/kuma/mongodb/bin/mongo localhost/kuma --quiet --eval 'db.resources.find({"kind":"search","name": "'$2'"}).forEach(function(doc){print(JSON.stringify(doc));});' | jq -c '._id="UUID" | .exportID="UUID" | .payload.id="UUID" | .payload.tenantID="T_ID" | .tenantID="T_ID" | .payload.clusterID="C_ID" | .payload.clusterIDs[]=["C_ID"] | .payload.eventSpaceConfig.spacesByClusterID={"C_ID":{"spaceids": [""],"allspaces":false}} | .userID="U_ID"'> $3
+		
+		if [[ $? -eq 0 ]]; then
+			echo -e "${GREEN}[OK] Search ${2} exported to file $3!${NC}"
+		else
+			echo -e "${RED}[FAILED] to export Search ${2} to file $3!${NC}"
+		fi
+	fi
+    ;;
+
+	"-importSearch")
+	if [[ ! $# -eq 2 ]] || [[ $2 == ""  ]] || [[ ! -f $2 ]] || [[ $(grep -c '"kind":"search"' "$2") -eq 0 ]]; then
+		echo -e "${YELLOW}Please enter valid arguments!\nExample: -importSearch \"My Search\" MySearch.json${NC}"
+		echo -e "${YELLOW}OR Please check file $2, is it exist?${NC}"
+	else
+		sed -i "s/T_ID/$$ACTUAL_TENANT_ID/g" $2
+		sed -i "s/U_ID/$$ACTUAL_ADMIN_ID/g" $2
+		sed -i "s/C_ID/$ACTUAL_CLUSTER_ID/g" $2
+		sed -i "s/UUID/$(cat /proc/sys/kernel/random/uuid)/g" $2
+
+		#if [[ cat $2 | grep -c '"kind":"search"' -eq 0 ]]; then
+		#	echo -e "$[FAILED] It is not Search file!${2}!${NC}"
+		#	exit 1
+		#fi
+		
+		/opt/kaspersky/kuma/mongodb/bin/mongo localhost/kuma --quiet --eval 'db.resources.insertOne('"$2"');'
+		
+		if [[ $? -eq 0 ]]; then
+			echo -e "${GREEN}[OK] Search from file ${2} imported!${NC}"
+		else
+			echo -e "${RED}[FAILED] to import Search ${2}!${NC}"
 		fi
 	fi
     ;;
